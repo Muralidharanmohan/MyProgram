2b7eecb54330acb7631c7588d9d6cb4c653a7db3
Muralidharanmohan <muralidharanbuddy@gmail.com>
2022-11-02 13:59:35 +0530

    GreenAddress

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensomr() {
+		System.out.println("greensOMR");
+	}
+	private void greensAdayar() {
+		System.out.println("greensAdayar");
+	}
+	private void greensTambaram() {
+		System.out.println("greensTambaram");
+	}
+	private void greensVelacherry() {
+		System.out.println("greensVelacherry");
+	}
+	private void greensAnnanagar() {
+		System.out.println("greensAnnanagar");
+	}
+	
+	public static void main(String[] args) {
+		
+		GreensTech G= new GreensTech();
+		
+		G.greensAdayar();
+		G.greensAnnanagar();
+		G.greensomr();
+		G.greensVelacherry();
+		G.greensTambaram();
+		
+	}
+	
+
+}
